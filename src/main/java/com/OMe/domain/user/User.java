eb98608d83a7e1036d24ea9838d429efe01dae93
Ray <55176622+kdl9405@users.noreply.github.com>
2021-01-04 17:12:33 +0900

    first commit

--- src/main/java/com/OMe/domain/user/User.java
+++ src/main/java/com/OMe/domain/user/User.java
@@ -22,22 +22,34 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @Column
+    private String picture;
+
     @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "character_id")
     private Character character;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 
     @Builder
-    public User(String name, String email){
+    public User(String name, String email , String picture ,Role role){
         this.name = name;
         this.email = email;
+        this.picture = picture;
+        this.role = role;
     }
 
-    public User update(String name, Character character){
+    public User update(String name, String picture){
         this.name = name;
-        this.character = character;
+        this.picture = picture;
 
         return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
     }
 
 
--- src/main/java/com/OMe/web/indexController.java
+++ src/main/java/com/OMe/web/indexController.java
@@ -1,16 +1,36 @@
 package com.OMe.web;
 
 
+import com.OMe.config.auth.LoginUser;
+import com.OMe.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @Controller
 public class indexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, @LoginUser SessionUser user)
+    {
+
+        if (user != null){
+            model.addAttribute("userName" , user.getName());
+        }
+
         return "index";
+    }
+
+    @GetMapping("/map")
+    public String map(Model model, @LoginUser SessionUser user) {
+
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "map3";
     }
 
 }
